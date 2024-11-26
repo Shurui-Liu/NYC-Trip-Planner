1fe4ae8899229b94925817f5f96add842a0112ac
Shurui-Liu <shurui.liu.710@gmail.com>
2024-11-26 12:51:49 -0800

    started the graph builder

---
+++ graph_builder.py
@@ -0,0 +1,11 @@
+"""
+This file builds a graph of the places user wants to visit in a day, together with
+starting and ending points.
+
+Representation: Adjacency List
+"""
+def graph_initializer():
+    """
+    Initializes the graph with the starting and ending points.
+    """
+    return
