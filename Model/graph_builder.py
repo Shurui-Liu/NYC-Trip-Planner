ef77dfd99677d33be4207816f072a5524b0b533f
Shurui-Liu <shurui.liu.710@gmail.com>
2024-11-27 20:13:45 -0800

    updated the create_graph func

--- Model/graph_builder.py
+++ Model/graph_builder.py
@@ -16,13 +16,21 @@ def calculate_distance(point1, point2) -> float:
     """
     return
 
-def graph_initializer(vertices) -> list:
+def create_graph(vertices: list) -> list:
     """
-    Initializes the graph with the starting and ending points.
+    Creates a graph with starting point, ending point and attractions
     Input: 
         - Vertices: Starting point, Ending point, Attractions to visit
     Output:
-        - Adjacency Matrix
-        - where 
+        - Adjacency Matrix representing the graph
     """
-    return
+    # initialize an empty adjacency matrix. where distances are 0.
+    adj_matrix = [[0 for _ in range(len(vertices))] for _ in range(len(vertices))]
+
+    # fill in the distance from each point to every other point
+    # time complexity: O(n^2)
+    for i in range(len(vertices)):
+        for j in range(i+1, len(vertices)):
+            adj_matrix[i][j] = calculate_distance(vertices[i], vertices[j])
+            adj_matrix[j][i] = adj_matrix[i][j]
+    return adj_matrix
