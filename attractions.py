c7e49ae707b339e929f08c61eec13579817916b2
Shurui-Liu <shurui.liu.710@gmail.com>
2024-12-05 23:41:25 -0800

    added attractions to dataset

---
+++ attractions.py
@@ -0,0 +1,252 @@
+attractions = {
+    "1": {
+        "name": "Statue of Liberty",
+        "time_length": 3,
+        "location": [40.689247, -74.044502]
+    },
+    "2": {
+        "name": "Central Park",
+        "time_length": 2.5,
+        "location": [40.785091, -73.968285]
+    },
+    "3": {
+        "name": "Empire State Building",
+        "time_length": 1.5,
+        "location": [40.748817, -73.985428]
+    },
+    "4": {
+        "name": "Times Square",
+        "time_length": 1,
+        "location": [40.758896, -73.985130]
+    },
+    "5": {
+        "name": "Brooklyn Bridge",
+        "time_length": 1.5,
+        "location": [40.706086, -73.996864]
+    },
+    "6": {
+        "name": "Metropolitan Museum of Art",
+        "time_length": 3,
+        "location": [40.779437, -73.963244]
+    },
+    "7": {
+        "name": "9/11 Memorial & Museum",
+        "time_length": 3,
+        "location": [40.711449, -74.013855]
+    },
+    "8": {
+        "name": "The High Line",
+        "time_length": 2,
+        "location": [40.747993, -74.004765]
+    },
+    "9": {
+        "name": "American Museum of Natural History",
+        "time_length": 3,
+        "location": [40.781324, -73.973988]
+    },
+    "10": {
+        "name": "Top of The Rock",
+        "time_length": 1.5,
+        "location": [40.759101, -73.979681]
+    },
+    "11": {
+        "name": "The Museum of Modern Art",
+        "time_length": 2,
+        "location": [40.761436, -73.977621]
+    },
+    "12": {
+        "name": "Grand Central Terminal",
+        "time_length": 1,
+        "location": [40.752726, -73.977229]
+    },
+    "13": {
+        "name": "New York Botanical Garden",
+        "time_length": 2.5,
+        "location": [40.862388, -73.880071]
+    },
+    "14": {
+        "name": "St. Patrick's Cathedral",
+        "time_length": 1,
+        "location": [40.758465, -73.975993]
+    },
+    "15": {
+        "name": "Rockefeller Center",
+        "time_length": 1.5,
+        "location": [40.758740, -73.978674]
+    },
+    "16": {
+        "name": "Washington Square Park",
+        "time_length": 1,
+        "location": [40.730823, -73.997332]
+    },
+    "17": {
+        "name": "Flatiron Building",
+        "time_length": 0.5,
+        "location": [40.741061, -73.989699]
+    },
+    "18": {
+        "name": "The Bronx Zoo",
+        "time_length": 4,
+        "location": [40.850596, -73.876998]
+    },
+    "19": {
+        "name": "One World Trade Center",
+        "time_length": 1.5,
+        "location": [40.712742, -74.013382]
+    },
+    "20": {
+        "name": "Vessel",
+        "time_length": 1,
+        "location": [40.754060, -74.002970]
+    },
+    "21": {
+        "name": "Fifth Avenue",
+        "time_length": 2,
+        "location": [40.775036, -73.965759]
+    },
+    "22": {
+        "name": "Carnegie Hall",
+        "time_length": 1.5,
+        "location": [40.765125, -73.979924]
+    },
+    "23": {
+        "name": "Bryant Park",
+        "time_length": 1,
+        "location": [40.753597, -73.983233]
+    },
+    "24": {
+        "name": "Madison Square Garden",
+        "time_length": 2,
+        "location": [40.750504, -73.993439]
+    },
+    "25": {
+        "name": "The Cloisters",
+        "time_length": 2,
+        "location": [40.864850, -73.931500]
+    },
+    "26": {
+        "name": "Intrepid Sea, Air & Space Museum",
+        "time_length": 2,
+        "location": [40.764527, -73.999608]
+    },
+    "27": {
+        "name": "Lincoln Center",
+        "time_length": 2,
+        "location": [40.772464, -73.983489]
+    },
+    "28": {
+        "name": "Chrysler Building",
+        "time_length": 0.5,
+        "location": [40.751620, -73.975502]
+    },
+    "29": {
+        "name": "Wall Street",
+        "time_length": 1,
+        "location": [40.707491, -74.011276]
+    },
+    "30": {
+        "name": "Battery Park",
+        "time_length": 1.5,
+        "location": [40.703277, -74.017028]
+    },
+    "31": {
+        "name": "New York Public Library",
+        "time_length": 1.5,
+        "location": [40.753182, -73.982253]
+    },
+    "32": {
+        "name": "Radio City Music Hall",
+        "time_length": 1.5,
+        "location": [40.759976, -73.979977]
+    },
+    "33": {
+        "name": "Chelsea Market",
+        "time_length": 1.5,
+        "location": [40.742439, -74.006191]
+    },
+    "34": {
+        "name": "Little Italy",
+        "time_length": 1.5,
+        "location": [40.719141, -73.997327]
+    },
+    "35": {
+        "name": "Chinatown",
+        "time_length": 1.5,
+        "location": [40.715751, -73.997031]
+    },
+    "36": {
+        "name": "SoHo",
+        "time_length": 2,
+        "location": [40.723301, -74.002988]
+    },
+    "37": {
+        "name": "Coney Island",
+        "time_length": 3,
+        "location": [40.575545, -73.970701]
+    },
+    "38": {
+        "name": "Governors Island",
+        "time_length": 2,
+        "location": [40.689450, -74.016792]
+    },
+    "39": {
+        "name": "Brooklyn Botanic Garden",
+        "time_length": 2,
+        "location": [40.667622, -73.962455]
+    },
+    "40": {
+        "name": "DUMBO",
+        "time_length": 1.5,
+        "location": [40.703316, -73.990160]
+    },
+    "41": {
+        "name": "Apollo Theater (Harlem)",
+        "time_length": 1.5,
+        "location": [40.810092, -73.950374]
+    },
+    "42": {
+        "name": "Sylvia's Restaurant (Harlem)",
+        "time_length": 1,
+        "location": [40.808736, -73.944922]
+    },
+    "43": {
+        "name": "Bushwick Collective (Street Art)",
+        "time_length": 2,
+        "location": [40.707751, -73.923528]
+    },
+    "44": {
+        "name": "Smorgasburg (Brooklyn Food Market)",
+        "time_length": 2,
+        "location": [40.721473, -73.957995]
+    },
+    "45": {
+        "name": "Ellis Island",
+        "time_length": 3,
+        "location": [40.699475, -74.039559]
+    },
+    "46": {
+        "name": "Roosevelt Island Tramway",
+        "time_length": 1,
+        "location": [40.761437, -73.953819]
+    },
+    "47": {
+        "name": "Catacombs of St. Patrick's Old Cathedral",
+        "time_length": 1.5,
+        "location": [40.723633, -73.995509]
+    },
+    "48": {
+        "name": "Edge Observation Deck",
+        "time_length": 1.5,
+        "location": [40.754260, -74.000620]
+    },
+    "49": {
+        "name": "The Strand Bookstore",
+        "time_length": 1,
+        "location": [40.733259, -73.990663]
+    },
+    "50": {
+        "name": "McSorley's Old Ale House",
+        "time_length": 1,
+        "location": [40.728633, -73.989350]
+    }
+}
