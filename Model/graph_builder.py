8bdee2ba7735a8b03693cd7b8605972b56eab4a0
Shurui-Liu <shurui.liu.710@gmail.com>
2024-12-07 14:50:43 -0800

    changed params: vertices to places for better naming

--- Model/graph_builder.py
+++ Model/graph_builder.py
@@ -7,21 +7,21 @@ Representation: Adjacency Matrix
 
 from functions import calculate_distance
 
-def create_graph(vertices: list) -> list:
+def create_graph(places: list) -> list:
     """
     Creates a graph with starting point, ending point and attractions
     Args: 
-        - Vertices: Starting point, Ending point, Attractions to visit
+        - places (list): a list of Starting point, Ending point, Attractions to visit
     Returns:
         - Adjacency Matrix representing the graph
     """
     # initialize an empty adjacency matrix. where distances are 0.
-    adj_matrix = [[0 for _ in range(len(vertices))] for _ in range(len(vertices))]
+    adj_matrix = [[0 for _ in range(len(places))] for _ in range(len(places))]
 
     # fill in the distance from each point to every other point
     # time complexity: O(n^2)
-    for i in range(len(vertices)):
-        for j in range(i+1, len(vertices)):
-            adj_matrix[i][j] = calculate_distance(vertices[i], vertices[j])
+    for i in range(len(places)):
+        for j in range(i+1, len(places)):
+            adj_matrix[i][j] = calculate_distance(places[i], places[j])
             adj_matrix[j][i] = adj_matrix[i][j]
     return adj_matrix
