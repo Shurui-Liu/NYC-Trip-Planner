bfe89c454c877e71522f7fe85f409f807c8fee7d
Shurui-Liu <shurui.liu.710@gmail.com>
2024-12-08 14:11:29 -0800

    added place_name_to_id_through_api

--- View/view_helpers.py
+++ View/view_helpers.py
@@ -46,3 +46,34 @@ def attractions_id_to_name(place_id: str, attractions: dict) -> str:
         if attractions[place_id] == place_id:
             return attractions[place_id]["name"]
     raise ValueError(f"Error: Place ID not found: {place_id}")
+
+def place_name_to_id_through_api(name: str, api_key: str) -> str:
+    """
+    Fetches the place_id of the place given the name. This is for 
+    starting and ending points, because they are not in the ATTRACTIONS dictionary.
+
+    Args:
+        place_id (str): The place_id of the attraction.
+        api_key (str): Google Places API key.
+
+    Returns:
+        str: place_id
+    """
+    url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json"
+    params = {
+        "input": name,
+        "inputtype": "textquery",
+        "fields": "place_id",
+        "key": api_key
+    }
+
+    response = requests.get(url, params=params)
+    
+    if response.status_code == 200:
+        data = response.json()
+        if data.get("candidates"):
+            return data["candidates"][0]["place_id"]
+        else:
+            raise ValueError(f"Error: Place does not exist: {name}")
+    else:
+        raise ValueError(f"Error: Unable to fetch data: {response.status_code}")
