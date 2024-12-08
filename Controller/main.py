abf32cce514bb71b31527e9e88fc744a43be6175
Shurui-Liu <shurui.liu.710@gmail.com>
2024-12-08 14:43:56 -0800

    added main including a blueprint in docstring

--- Controller/main.py
+++ Controller/main.py
@@ -8,5 +8,8 @@ from View import user_interaction_functions as uif
 def main():
     """
     1. Take user input -> max_daily_time, attractions_info, starting_name, ending_name
-
+    2. Get place_ids for starting_name and ending_name
+    3. Build a graph of places (attractions + starting & ending points) using the place_ids
+    4. Plan the trip using the graph -> optimized_path (a list of place_ids)
+    5. Display the trip plan
     """
