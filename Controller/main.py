ea6b1696b5eac2042b3b4666e13dbe9b4e3b160f
Shurui-Liu <shurui.liu.710@gmail.com>
2024-12-08 14:43:02 -0800

    added main

---
+++ Controller/main.py
@@ -0,0 +1,12 @@
+"""main function for the NYC Trip Planner"""
+
+PLACES_API = "AIzaSyDafDzPc6c8ODZ0LZMcOYJrlvw7jgZmDeo"
+
+from attractions import ATTRACTIONS
+from View import user_interaction_functions as uif
+
+def main():
+    """
+    1. Take user input -> max_daily_time, attractions_info, starting_name, ending_name
+
+    """
