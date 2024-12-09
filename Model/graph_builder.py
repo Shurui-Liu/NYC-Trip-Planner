0c99165f3d44de5cd9b8b503442dfacd10d5caea
Shurui-Liu <shurui.liu.710@gmail.com>
2024-12-09 13:01:02 -0800

    updated create_graph

--- Model/graph_builder.py
+++ Model/graph_builder.py
@@ -5,7 +5,7 @@ starting and ending points.
 Representation: Adjacency Matrix
 """
 
-from Model import functions
+import functions
 
 
 def create_graph(gmaps, places: list) -> list:
@@ -15,6 +15,13 @@ def create_graph(gmaps, places: list) -> list:
         - places (list): a list of place_ids of Starting point, Ending point, Attractions to visit
     Returns:
         - Adjacency Matrix representing the graph
+    
+    Example:
+        >>> import googlemaps
+        >>> gmaps = googlemaps.Client(key="AIzaSyDafDzPc6c8ODZ0LZMcOYJrlvw7jgZmDeo")
+        >>> places = ['ChIJ4zGFAZpYwokRGUGph3Mf37k', 'ChIJaXQRs6lZwokRY6EFpJnhNNE']
+        >>> create_graph(gmaps, places)
+        [[0, 2.238], [2.238, 0]]
     """
     # initialize an empty adjacency matrix. where distances are 0.
     adj_matrix = [[0 for _ in range(len(places))] for _ in range(len(places))]
@@ -26,3 +33,8 @@ def create_graph(gmaps, places: list) -> list:
             adj_matrix[i][j] = functions.calculate_distance(gmaps, places[i], places[j])
             adj_matrix[j][i] = adj_matrix[i][j]
     return adj_matrix
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
